69d900dad1bc5a2db5fa8add044c4dc31b998dd8
Elham Dermaku <dermaku12@gmail.com>
2023-10-18 00:39:51 +0200

    add and fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {TypeAnimation} from 'react-type-animation';
-import {FaTwitter, FaFacebook, FaInstagram, FaLinkedinIn, FaGithub} from 'react-icons/fa';
+import {FaTwitter, FaFacebook, FaLinkedinIn, FaGithub} from 'react-icons/fa';
 
 const Main = () => {
   return (
@@ -11,14 +11,13 @@ const Main = () => {
         alt="/" 
       />
       <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
-      <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
+      <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-center items-center'>
         <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>I'm Elham Dërmaku</h1>
         <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>
           I'm a
           <TypeAnimation
             sequence={[
-              // Same substring at the start will only be typed out once, initially
-              'Developer',
+              'Software Developer',
               2000, 
               'Coder',
               2000,
@@ -33,10 +32,14 @@ const Main = () => {
           />
         </h2>
         <div className='flex justify-between pt-6 max-w-[200px] w-full'>
-          <FaTwitter className='cursor-pointer' size={20}/>
+          <FaTwitter className='cursor-pointer' size={20} />
           <FaFacebook className='cursor-pointer' size={20}/>
-          <FaGithub className='cursor-pointer' size={20}/>
-          <FaLinkedinIn className='cursor-pointer' size={20}/>
+          <a href="https://github.com/ElhamDe">
+            <FaGithub className='cursor-pointer' size={20}/>
+          </a>
+          <a href="https://www.linkedin.com/in/elham-dermaku/">
+            <FaLinkedinIn className='cursor-pointer' size={20}/>
+          </a>
         </div>
       </div>
       </div>
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,10 +4,11 @@ import WorkItem from './WorkItem';
 const data = [
   {
     year: 2023,
-    title: 'Fullstack Developer',
+    title: 'Fullstack Developer Intern',
     duration: '3 Months',
+
     details:
-    'Lorem ipsun, dasiojd asiojdo iasiod oasj asiojd ioasjdo ijasiojd asiojd ioasjido ajsiod jasiojdio asjioasjdioasd'
+    'I was in an internship at Starlabs - Prishtine, Kosovë from 01.06.2023 – 01.09.2023. Collaborated with a cross-functional team to design and develop responsive web applications using mainly React.js for Frontend Node.js Backend and for Database MongoDB. Integrated dynamic features and user interfaces to enhance user experience. Conducted thorough testing and debugging to ensure smooth functionality and optimal performance. Engaged in code reviews, offering valuable feedback to improve code quality.'
   },
   {
     year: 2024,
@@ -21,7 +22,7 @@ const data = [
 const Work = () => {
   return (
     <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
-      <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
+      <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work Experience</h1>
       {data.map((item, idx) => (
         <WorkItem 
           key={idx} 
