dc68bc9a5f1ff7753c9718a78bf2eed5cd13e479
Elham Dermaku <dermaku12@gmail.com>
2023-10-18 14:53:39 +0200

    add changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,25 +21,25 @@ const Projects = () => {
         <ProjectItems 
           img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43654066-c3f9fbad7e9fe3727d06ca37cd4fbec9.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T120655Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=6468c6b69d55f47234df6bacdc1d48dce8490de948808810f94934d478e74176' 
           title='Rent Property Website' 
-          link='https://inventory-internship.netlify.app/'
+          link='https://elham-rent-a-home.netlify.app'
           technology='React JS'
         />
         <ProjectItems 
           img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43653964-95efef247a52bf35b0ca3f5719f0d531.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T120204Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=37bd65b8224833a3352881765a6bab126932c22c3bd5cd9a6a2070fd01b8e1ad' 
           title='E-Commer Website'
-          link='https://inventory-internship.netlify.app/'
+          link='https://e-commerce-elham.netlify.app/'
           technology='React JS'
         />
         <ProjectItems 
           img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43654013-7a058cb2574b4746ecdc0f1fda05f90d.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T120451Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=c03bf999210a725e002685b4a04948fae7ec6a69c025df97c4bc61f41c9ae53b' 
           title='HR Website' 
-          link='https://inventory-internship.netlify.app/'
+          link='https://hr-website-elham.netlify.app/'
           technology='HTML, CSS, JS'
         />
         <ProjectItems 
           img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43654509-6e56ad482a46db240e0ef4a69124ea1d.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T122557Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=063de40c900813493ae16fb270b5a1a220cea92a2081c0a628623af9701d92b2' 
           title='Summarize Article AI Website' 
-          link='https://inventory-internship.netlify.app/'
+          link='https://summarize-article-ai.netlify.app/'
           technology='React JS API'
         />
       </div>
