e83b5f5eb317852897bf4d17d028189e0e6e071a
Elham Dermaku <dermaku12@gmail.com>
2023-10-18 14:50:17 +0200

    add changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,10 +12,36 @@ const Projects = () => {
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi nesciunt quibusdam odio voluptatum porro quae quis labore quia temporibus dolorem?
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
-        <ProjectItems img='https://blog.hubspot.com/hs-fs/hubfs/ecommerce%20marketing.jpg?width=595&height=400&name=ecommerce%20marketing.jpg' title='Amazon App' />
-        <ProjectItems img='https://blog.hubspot.com/hs-fs/hubfs/ecommerce%20marketing.jpg?width=595&height=400&name=ecommerce%20marketing.jpg' title='Amazon App' />
-        <ProjectItems img='https://blog.hubspot.com/hs-fs/hubfs/ecommerce%20marketing.jpg?width=595&height=400&name=ecommerce%20marketing.jpg' title='Amazon App' />
-        <ProjectItems img='https://blog.hubspot.com/hs-fs/hubfs/ecommerce%20marketing.jpg?width=595&height=400&name=ecommerce%20marketing.jpg' title='Amazon App' />
+        <ProjectItems 
+          img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43654441-bb8418e214c348555458475d44f10a8a.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T122255Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=74ad94f58016249b08aa7e3bad1b3b9162b881d01f2fa4886e497b6ab768d103' 
+          title='Inventory Management Website' 
+          link='https://inventory-internship.netlify.app/'
+          technology='React JS, Node JS'
+        />
+        <ProjectItems 
+          img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43654066-c3f9fbad7e9fe3727d06ca37cd4fbec9.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T120655Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=6468c6b69d55f47234df6bacdc1d48dce8490de948808810f94934d478e74176' 
+          title='Rent Property Website' 
+          link='https://inventory-internship.netlify.app/'
+          technology='React JS'
+        />
+        <ProjectItems 
+          img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43653964-95efef247a52bf35b0ca3f5719f0d531.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T120204Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=37bd65b8224833a3352881765a6bab126932c22c3bd5cd9a6a2070fd01b8e1ad' 
+          title='E-Commer Website'
+          link='https://inventory-internship.netlify.app/'
+          technology='React JS'
+        />
+        <ProjectItems 
+          img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43654013-7a058cb2574b4746ecdc0f1fda05f90d.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T120451Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=c03bf999210a725e002685b4a04948fae7ec6a69c025df97c4bc61f41c9ae53b' 
+          title='HR Website' 
+          link='https://inventory-internship.netlify.app/'
+          technology='HTML, CSS, JS'
+        />
+        <ProjectItems 
+          img='https://awesomescreenshot.s3.amazonaws.com/image/4673379/43654509-6e56ad482a46db240e0ef4a69124ea1d.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJSCJQ2NM3XLFPVKA%2F20231018%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20231018T122557Z&X-Amz-Expires=28800&X-Amz-SignedHeaders=host&X-Amz-Signature=063de40c900813493ae16fb270b5a1a220cea92a2081c0a628623af9701d92b2' 
+          title='Summarize Article AI Website' 
+          link='https://inventory-internship.netlify.app/'
+          technology='React JS API'
+        />
       </div>
     </div>
   )
